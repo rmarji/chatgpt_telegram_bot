35b1c9da796b2f5ee8cdb071c0cc008b52df39d0
rmarji <raed@jogeeks.com>
2023-05-02 04:04:06 +0000

    Updated environment

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,7 @@
 import yaml
 import dotenv
 from pathlib import Path
+import os
 
 config_dir = Path(__file__).parent.parent.resolve() / "config"
 
@@ -12,8 +13,8 @@ with open(config_dir / "config.yml", 'r') as f:
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
+telegram_token = os.environ["telegram_token"]
+openai_api_key = os.environ["openai_api_key"]
 use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
